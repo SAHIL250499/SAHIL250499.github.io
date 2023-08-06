61ecf67261ac5cbb019bfb766777d1a53ef77e63
SAHIL250499 <samp9270@gmail.com>
2023-08-06 14:46:35 +0530

    Changed size of computer model for mobile screen

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -22,8 +22,8 @@ const Computers = ({ isMobile }) => {
       <pointLight intensity={5} />
       <primitive
         object={computer.scene}
-        scale={isMobile ? 0.7 : 0.75}
-        position={isMobile ? [0, -3, -2.2] : [0, -3.85, -1.5]}
+        scale={isMobile ? 0.45 : 0.75}
+        position={isMobile ? [0, -3, -1.3] : [0, -3.85, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
